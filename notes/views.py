0202ee203e04746bb1c15aa9953b09689b46824a
Nabin <nabin.sah.9817@gmail.com>
2025-12-28 07:56:37 +0545

    final submition version of app

--- notes/views.py
+++ notes/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
-from django.core.mail import send_mail
 
 from rest_framework import generics, permissions, filters, status
 from rest_framework.views import APIView
